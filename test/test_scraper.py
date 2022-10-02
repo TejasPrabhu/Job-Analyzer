ef145f00f3308816395679ce3b2b86dab87b6ec0
Samarth <samarthp1@gmail.com>
2022-10-02 16:56:13 -0400

    Added basic test for selenium with github actions

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,6 +1,40 @@
-def test_sample():
-    assert True
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 
-def test_sample2():
-    assert False
+def setup():
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--start-maximized",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+    return driver
+
+
+def test_scraper():
+    url = "https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=Raleigh%2C%20North%20Carolina" \
+          "%2C%20United%20States&geoId=100197101&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+
+    wd = setup()
+    wd.get(url)
+    jobs = wd.find_element(By.CLASS_NAME, 'jobs-search__results-list').find_elements(By.CLASS_NAME, 'job-search-card')
+    wd.quit()
+    print("Number of jobs {}".format(len(jobs)))
+
+    assert len(jobs) > 0
