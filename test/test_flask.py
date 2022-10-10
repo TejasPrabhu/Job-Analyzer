f5f9383c74dcc8620a17e12b1213962c16e7628c
Naveen-Jayanna <naveen.jayanna1@gmail.com>
2022-10-09 20:20:00 -0400

    Added flask test #2

--- test/test_flask.py
+++ test/test_flask.py
@@ -6,3 +6,13 @@ def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert b"Get Job Postings" in response.data
+
+
+def test_search_page():
+    response = app.test_client().get('/search')
+    assert response.status_code == 200
+    assert b"Job Title" in response.data
+    assert b"Location" in response.data
+    assert b"Company Name" in response.data
+    assert b"Technical skills" in response.data
+    assert b"Job Type" in response.data
