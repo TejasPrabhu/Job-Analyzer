7d5aace255a45f40597d67c89bb2ab44c4294770
ameyagv <ameyav1234@gmail.com>
2022-11-28 15:48:04 -0500

    update user models

---
+++ src/User/routes.py
@@ -0,0 +1,17 @@
+from src.app import app
+from src import User
+
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+    return User().signup()
+
+
+@app.route('/user/signout')
+def signout():
+    return User().signout()
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+    return User().login()
