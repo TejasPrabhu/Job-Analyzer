8367090da05bc197e76408d45574f9766bf65744
subodhgujar <subodhsgujar@gmail.com>
2022-12-04 13:06:01 -0500

    updating chrome driver path

--- src/scraper.py
+++ src/scraper.py
@@ -50,7 +50,7 @@ class JobData:
         """
         The function setup_webdriver sets the options of Chrome Driver and creates webdriver.Chrome object.
         """
-        chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+        # chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
         chrome_options = Options()
         options = [
@@ -65,11 +65,11 @@ class JobData:
         for option in options:
             chrome_options.add_argument(option)
 
-        self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        # driver_path = r"../webdriver/chromedriver"
-        # if os.name != "posix":
-        #     driver_path = driver_path + ".exe"
-        # self.driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+        # self.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        driver_path = r"./webdriver/chromedriver"
+        if os.name != "posix":
+            driver_path = driver_path + ".exe"
+        self.driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
 
     def scroll_to_end(self):
         """
